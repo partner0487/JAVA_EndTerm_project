b12f1d13c01a59985a812ceca416b73b21917377
xiaoyo123 <36474975azl@gmail.com>
2023-05-12 02:51:25 +0800

    我放棄用那個jh lab了

--- imageFilter.java
+++ imageFilter.java
@@ -49,7 +49,7 @@ public class imageFilter extends JFrame {
         filter.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 imageData = getImageData(fileName[0]);
-                BlackWhite(getGraphics());
+                old(getGraphics());
             }
         });
 
@@ -127,6 +127,23 @@ public class imageFilter extends JFrame {
                 B = color.getBlue();
                 int gray = (R + G + B) / 3;
                 g.setColor(new Color(gray, gray, gray));
+                g.fillRect(100 + i, 100 + j, 1, 1);
+            }
+        }
+    }
+    public void old(Graphics g) {
+        super.paint(g);
+
+        int rgb, R, G, B;
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                rgb = imageData[i][j];
+                Color color = new Color((int)rgb);
+                R = (int)(color.getRed() * 0.393 + color.getGreen() * 0.769 + color.getBlue() * 0.189);
+                G = (int)(color.getRed() * 0.349 + color.getGreen() * 0.686 + color.getBlue() * 0.168);
+                B = (int)(color.getRed() * 0.272 + color.getGreen() * 0.534 + color.getBlue() * 0.131);
+                R = R > 255 ? 255 : R; G = G > 255 ? 255 : G; B = B > 255 ? 255 : B;
+                g.setColor(new Color(R, G, B));
                 g.fillRect(100 + i, 100 + j, 1, 1);
             }
         }
